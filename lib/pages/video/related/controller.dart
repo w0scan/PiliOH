16a471152703255b3e7ac13f3b626b62ef6dfdb1
w s <wsheng128@gmail.com>
2026-07-15 21:13:31 +0800

    feat: 相关视频末尾追加标题搜索结果
    在播放页面相关视频列表末尾，追加使用当前视频标题搜索到的视频结果。
    搜索结果会过滤掉当前视频本身，并应用推荐过滤设置。
    将SearchVideoItemModel转换为HotVideoItemModel以保持列表类型一致。

--- lib/pages/video/related/controller.dart
+++ lib/pages/video/related/controller.dart
@@ -1,13 +1,19 @@
 import 'package:PiliPlus/http/loading_state.dart';
+import 'package:PiliPlus/http/search.dart';
 import 'package:PiliPlus/http/video.dart';
+import 'package:PiliPlus/models/common/search/search_type.dart';
 import 'package:PiliPlus/models/model_hot_video_item.dart';
+import 'package:PiliPlus/models/model_owner.dart';
+import 'package:PiliPlus/models/search/result.dart';
 import 'package:PiliPlus/pages/common/common_list_controller.dart';
+import 'package:PiliPlus/utils/recommend_filter.dart';
 import 'package:get/get.dart';
 
 class RelatedController
     extends CommonListController<List<HotVideoItemModel>?, HotVideoItemModel> {
   RelatedController({this.autoQuery = true});
   String bvid = Get.arguments['bvid'];
+  String? title = Get.arguments['title'];
   final bool autoQuery;
 
   @override
@@ -21,4 +27,69 @@ class RelatedController
   @override
   Future<LoadingState<List<HotVideoItemModel>?>> customGetData() =>
       VideoHttp.relatedVideoList(bvid: bvid);
+
+  @override
+  Future<void> queryData([bool isRefresh = true]) async {
+    await super.queryData(isRefresh);
+    if (isRefresh && title != null && title!.isNotEmpty) {
+      await _searchByTitle();
+    }
+  }
+
+  Future<void> _searchByTitle() async {
+    final res = await SearchHttp.searchByType<SearchVideoData>(
+      searchType: SearchType.video,
+      keyword: title!,
+      page: 1,
+      onSuccess: (_) {},
+    );
+    if (res case Success(:final response)) {
+      final searchItems = response.list;
+      if (searchItems == null || searchItems.isEmpty) return;
+
+      final hotItems = searchItems
+          .where((item) => item.bvid != bvid)
+          .map(_searchToHotVideo)
+          .toList();
+
+      final filteredItems = RecommendFilter.applyFilterToRelatedVideos
+          ? hotItems.where((i) => !RecommendFilter.filterAll(i)).toList()
+          : hotItems;
+
+      if (filteredItems.isEmpty) return;
+
+      if (loadingState.value case Success(:final response)) {
+        response?.addAll(filteredItems);
+        loadingState.refresh();
+      }
+    }
+  }
+
+  HotVideoItemModel _searchToHotVideo(SearchVideoItemModel search) {
+    final searchOwner = search.owner;
+    final searchStat = search.stat;
+    return HotVideoItemModel.fromJson({
+      'aid': search.aid,
+      'bvid': search.bvid,
+      'pic': search.cover,
+      'title': search.title,
+      'pubdate': search.pubdate,
+      'ctime': search.ctime,
+      'desc': search.desc,
+      'duration': search.duration,
+      'owner': {
+        'mid': searchOwner.mid,
+        'name': searchOwner.name,
+        'face': searchOwner is Owner ? searchOwner.face : null,
+      },
+      'stat': {
+        'view': searchStat.view,
+        'danmaku': searchStat.danmu,
+        'like': searchStat.like,
+        'reply': searchStat is SearchStat ? searchStat.reply : null,
+        'favorite': searchStat is SearchStat ? searchStat.favorite : null,
+      },
+      'redirect_url': search.redirectUrl,
+    });
+  }
 }
